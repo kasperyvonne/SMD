f42a7312d583256fea4daf957868c0bac21f7727
Yvonne Kasper <yvonne.kasper@tu-dortmund.de>
2018-11-07 12:51:06 +0100

    kleine Aenderungen

--- Blatt3/plots/Aufgabe1.py
+++ Blatt3/plots/Aufgabe1.py
@@ -1,8 +1,9 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy import optimize
+import timeit
 
-
+# Definitionen und so
 def Planck(x):
     return(15/(np.pi)**4)*(x**3)/(np.exp(x)-1)
 
@@ -17,22 +18,56 @@ def SchnittMajo(x, ymax):
     return (ymax-200*(15/(np.pi)**4) * (x**(-0.1)) * (np.exp(-x**(0.9))))
 
 
-def Majorante(x):
-    # if (x <= xs):
-    #     return(ymax)
-    #else:
+def Majorante2(x):
     return(200*(15/(np.pi)**4) * (x**(-0.1)) * (np.exp(-x**(0.9))))
 
 
+def Majorante1(x):
+    return(0.21888647009110665)
+
+
+# Vorbereitungsbums
 x = np.linspace(0.0001, 20, 10000)
 xmax = optimize.brentq(PlanckAbl, 2.5, 5)
 ymax = Planck(xmax)
 xs = optimize.brentq(SchnittMajo, 4.5, 6, args=(ymax))
+# plt.plot(x, Majorante2(x))
+# plt.plot(x, Planck(x))
+# plt.ylim(0, 0.3)
+# plt.axhline(y=ymax)
+# plt.savefig('Majoranten.png')
+# plt.clf()
+# Aufgabenteil a)
 
 
-print(xs)
-plt.plot(x, Majorante(x))
-plt.plot(x, Planck(x))
-plt.ylim(0,0.3)
-plt.axhline(y=ymax)
+def Rueckweisung():
+    zaehler = 0
+    Zufallszahlen = np.empty((100000, 2))
+    while zaehler < 100000:
+        xwert = np.random.uniform(0, 20)
+        ywert = np.random.uniform(0, ymax)
+        if(ywert <= Planck(xwert)):
+            Zufallszahlen[zaehler] = [xwert, ywert]
+            zaehler += 1
+    return(Zufallszahlen)
+
+
+TEST_RUECKWEISUNG = '''
+def Rueckweisung():
+    zaehler = 0
+    Zufallszahlen = np.empty((100000, 2))
+    while zaehler < 100000:
+        xwert = np.random.uniform(0, 20)
+        ywert = np.random.uniform(0, ymax)
+        if(ywert <= Planck(xwert)):
+            Zufallszahlen[zaehler] = [xwert, ywert]
+            zaehler += 1
+    return(Zufallszahlen)
+'''
+
+Zeit = timeit.timeit(TEST_RUECKWEISUNG, number=1)
+Zahlen = Rueckweisung()
+plt.hist(Zahlen[:, 0], bins=100)
 plt.show()
+#print(Zahlen[:,1])
+print(Zeit)
