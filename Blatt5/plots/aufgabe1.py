f11c1fa1999eef8730d3fdebb0d3ab1a8a0fd5ec
Yvonne Kasper <yvonne.kasper@tu-dortmund.de>
2018-11-19 20:12:12 +0100

    Text zu 1a und b

--- Blatt5/plots/aufgabe1.py
+++ Blatt5/plots/aufgabe1.py
@@ -15,6 +15,26 @@ def Akzeptanz(E):
     return((1-np.exp(-0.5*E))**3)
 
 
+def Polarmethode(E):
+    i = 0
+    x = -1
+    while i == 0:
+        u1 = np.random.uniform(0, 1)
+        u2 = np.random.uniform(0, 1)
+        v1 = 2*u1-1
+        v2 = 2*u2-1
+        s = v1**2+v2**2
+        if (s < 1):
+            term = np.sqrt(-(2/s)*np.log(s))
+            c1 = v1*term
+            c2 = v2*term
+            x = 2*E*c1+2*E*c2+10*E
+            if (x > 0):
+                x = np.round(x)
+                i = 1
+    return(x)
+
+
 # Aufgabenteil a)
 Esize = 100000
 x = np.random.uniform(0, 1, Esize)
@@ -30,14 +50,20 @@ for i in np.arange(0, Esize):
         Detektiert[i] = True
 
 DetEnergie = Energie[Detektiert]
-plt.hist(Energie, bins=np.logspace(0, 3), histtype='step', density='True',
+plt.hist(Energie, bins=np.logspace(0, 3), histtype='step',
          label='Energie')
 plt.hist(DetEnergie, bins=np.logspace(0, 3), histtype='step', linestyle='--',
-         density='True',label='Detektierte Energie')
+         label='Detektierte Energie')
 plt.loglog()
 plt.legend()
 plt.xlabel(r'$E/\mathrm{TeV}$')
 plt.ylabel(r'Ereignisse')
 plt.savefig('Energie.pdf')
-
+plt.clf()
 # Aufgabenteil c)
+Hits = np.zeros(len(DetEnergie), dtype=int)
+for j in np.arange(0, len(DetEnergie)):
+    Hits[j] = Polarmethode(DetEnergie[j])
+
+plt.hist(Hits, bins=50, range=[0, 100], histtype='step')
+plt.show()
