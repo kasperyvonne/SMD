557da7fe061c7bb08b87a44b32b6e13189b86aa3
Robubabu <robert.appel@udo.edu>
2018-12-06 14:40:11 +0100

    Aufgabe 3 Theorie gemacht und fertig

--- Blatt7/plots/Aufgabe3.py
+++ Blatt7/plots/Aufgabe3.py
@@ -6,36 +6,32 @@ from scipy.optimize import curve_fit
 from uncertainties import correlated_values, correlation_matrix
 from uncertainties import ufloat
 from uncertainties.unumpy import (nominal_values as noms, std_devs as stds)
-x = np.linspace(0, 10, 1000)
-mhub = const.value('Bohr magneton') #das gelibete Borhsche Magneton zeigt wie man Scipy Constants benutzt
-def mittel(x):              #the real mean()-ing of life
-    return ufloat(np.mean(x),np.std(x,ddof=1)/np.sqrt(len(x)))
-def relf(l,m):  #in Prozent
-    return (np.absolute(l-m)/l)*100
+import pandas as pd
+
+P_0 = pd.read_hdf('populationen.hdf5', key ='P_0')
+P_1 = pd.read_hdf('populationen.hdf5', key ='P_1')
+Pges = pd.DataFrame({
+	'x': np.append(P_0['x'],P_1['x']),
+	'y': np.append(P_0['y'],P_1['y']),
+	#'X': np.append(Pges['x'],Pges['y']),
+	#'classmask' : np.append(np.zeros(len(Pges['x'])),np.ones(len(Pges['y']))),
+	'label' : np.append(np.zeros(len(P_0)),np.ones(len(P_1))), })
+Wone = np.ones((2,len(Pges['X'])))
+bone = np.ones(2) 
+def ffunk(k,W,b):
+	if k == 0:
+		return np.dot(W.T[k],Pges['x']) + b[0]
+	else:
+		return np.dot(W.T[k],Pges['y']) + b[1]
+def softmax(k,i,W,b):
+	return np.exp(ffunk(W,x,b)) /(np.exp(W[
+for i in range(100):
+	
 
 
-#Fit
-params , cov = curve_fit(f , x ,y )
-params = correlated_values(params, cov)
-for p in params:
-    print(p)
+#plt.plot(x, y, label='Kurve')
+#plt.xlabel(r'$\alpha \:/\: \si{\ohm}$')
+#plt.ylabel(r'$y \:/\: \si{\micro\joule}$')
+#plt.legend(loc='best')
+#plt.savefig('A3plot.pdf')
 
-
-#Tabelle
-# np.savetxt('tab.txt',np.column_stack([x,y]), delimiter=' & ',newline= r'\\'+'\n' )
-#plt.subplot(1, 2, 1)
-plt.plot(x, y, label='Kurve')
-plt.xlabel(r'$\alpha \:/\: \si{\ohm}$')
-plt.ylabel(r'$y \:/\: \si{\micro\joule}$')
-plt.legend(loc='best')
-plt.savefig('build/plot.pdf')
-plt.clf()
-#plt.subplot(1, 2, 2)
-plt.plot(x, y, label='Kurve')
-plt.xlabel(r'$\alpha \:/\: \si{\ohm}$')
-plt.ylabel(r'$y \:/\: \si{\micro\joule}$')
-plt.legend(loc='best')
-
-# in matplotlibrc leider (noch) nicht möglich
-#plt.tight_layout(pad=0, h_pad=1.08, w_pad=1.08)
-plt.savefig('build/plot2.pdf')
